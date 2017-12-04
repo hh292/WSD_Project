34bab58f23e0f670238aa3bfd1c78726a21b9232
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-04 18:53:53 -0500

    model-account

---
+++ models/account.php
@@ -0,0 +1,29 @@
+<?php
+final class account extends \database\model
+{
+    public $id;
+    public $email;
+    public $fname;
+    public $lname;
+    public $phone;
+    public $birthday;
+    public $gender;
+    public $password;
+    protected static $modelName = 'account';
+    public static function getTablename()
+    {
+        $tableName = 'accounts';
+        return $tableName;
+    }
+    //to find a users tasks you need to create a method here.  Use $this->id to get the usersID For the query
+    public static function findTasks()
+    {
+        //I am temporarily putting a findall here but you should add a method to todos that takes the USER ID and returns their tasks.
+        $records = todos::findAll();
+        print_r($records);
+        return $records;
+    }
+    //add a method to compare the passwords this is where bcrypt should be done and it should return TRUE / FALSE for login
+    public static function checkPassword($password) {}
+}
+?>
