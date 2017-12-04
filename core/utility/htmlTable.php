eab8c6fbd89a55fcb8a0f53583043d388e74b833
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-04 18:50:08 -0500

    htmlTable

---
+++ core/utility/htmlTable.php
@@ -0,0 +1,49 @@
+<?php
+namespace utility;
+//namespace MyProject\mvcName;
+class htmlTable
+{
+    public static function genarateTableFromMultiArray($array)
+    {
+        $tableGen = '<table border="1"cellpadding="10">';
+        $tableGen .= '<tr>';
+        //this grabs the first element of the array so we can extract the field headings for the table
+        $fieldHeadings = $array[0];
+        $fieldHeadings = get_object_vars($fieldHeadings);
+        $fieldHeadings = array_keys($fieldHeadings);
+        //this gets the page being viewed so that the table routes requests to the correct controller
+        $referingPage = $_REQUEST['page'];
+        foreach ($fieldHeadings as $heading) {
+            $tableGen .= '<th>' . $heading . '</th>';
+        }
+        $tableGen .= '</tr>';
+        foreach ($array as $record) {
+            $tableGen .= '<tr>';
+            foreach ($record as $key => $value) {
+                if ($key == 'id') {
+                    $tableGen .= '<td><a href="index.php?page=' . $referingPage . '&action=show&id=' . $value . '">View</a></td>';
+                } else {
+                    $tableGen .= '<td>' . $value . '</td>';
+                }
+            }
+            $tableGen .= '</tr>';
+        }
+        $tableGen .= '</table>';
+        return $tableGen;
+    }
+    public static function generateTableFromOneRecord($innerArray)
+    {
+        $tableGen = '<table border="1" cellpadding="10"><tr>';
+        $tableGen .= '<tr>';
+        foreach ($innerArray as $innerRow => $value) {
+            $tableGen .= '<th>' . $innerRow . '</th>';
+        }
+        $tableGen .= '</tr>';
+        foreach ($innerArray as $value) {
+            $tableGen .= '<td>' . $value . '</td>';
+        }
+        $tableGen .= '</tr></table><hr>';
+        return $tableGen;
+    }
+}
+?>
