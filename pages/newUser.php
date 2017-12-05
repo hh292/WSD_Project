9849747801721d1398d920dc8f9c7f8b5f276ca2
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-05 10:25:59 -0500

    newUser page added

---
+++ pages/newUser.php
@@ -0,0 +1,55 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<?php
+//this is how you print something
+//print utility\htmlTable::genarateTableFromMultiArray($data);
+?>
+
+<form action="index.php?page=accounts&action=register" method="POST">
+
+    <div class="container">
+        <label><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" required></br></br>
+
+        <label><b>First Name</b></label>
+        <input type="text" placeholder="Enter First Name" name="fname" required></br></br>
+
+        <label><b>Last Name</b></label>
+        <input type="text" placeholder="Enter Last Name" name="lname" required></br></br>
+
+        <label><b>Phone No</b></label>
+        <input type="text" placeholder="Enter Phone number" name="phone" required></br></br>
+
+        <label><b>Birthday</b></label>
+        <input type="text" placeholder="Enter Birthday" name="bday" required></br></br>
+
+        <label><b>Gender</b></label>
+        <input type="text" placeholder="Enter Gender" name="gender" required></br></br>
+
+        <label><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required></br></br>
+
+        <button type="submit">Register</button></br></br>
+
+    </div>
+
+
+</form>
+
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
