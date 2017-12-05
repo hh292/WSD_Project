1dc46004d5723ee4aee352eec62133e60146e48b
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-04 19:01:38 -0500

    homepage

---
+++ pages/homepage.php
@@ -0,0 +1,48 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+
+<h1>
+    <?php
+    //this how to print some data;
+    echo $data['site_name'];
+    ?> </h1>
+
+<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
+<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
+
+<form action="index.php?page=accounts&action=login" method="POST">
+
+    <div class="container">
+        <label><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+
+        <label><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <button type="submit">Login</button>
+    </div>
+
+
+</form>
+
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
