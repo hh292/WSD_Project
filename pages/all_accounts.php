f18067abac8958dc35e2c695084e9c6ee3c6bb00
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-04 19:00:49 -0500

    all accounts

---
+++ pages/all_accounts.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<?php
+//this is how you print something
+print utility\htmlTable::genarateTableFromMultiArray($data);
+?>
+
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
