9de869e3fe9f1852da52b7c44de5bdc5214381e4
Himanshu Hunge <hh292@afsconnect1.njit.edu>
2017-12-05 12:17:36 -0500

    made changes

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,58 +1,43 @@
-<!doctype html>
-
-<html lang="en">
+<!DOCTYPE html>
+<html lang="en"> 
 <head>
-    <meta charset="utf-8">
-
-    <title>WSD Final Project</title>
-    <meta name="description" content="WSD Final Project">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>WSD Project - Fall2017</title>
+  <link rel="stylesheet" href="style.css">
+  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
-
 <body>
-
-
-<h1>
-    <?php
-    //this how to print some data;
-    //echo $data['site_name'];
-    ?> </h1>
-
-<!--<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
-<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>-->
-
-<form action="index.php?page=accounts&action=login" method="POST">
-
-    <div class="container">
-        <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required></br></br>
-
-        <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required></br></br>
-
-        <button type="submit">Login</button></br></br>
-
+  <section class="container">
+    <div class="login">
+      <h1>Login to Web Application</h1>
+      <form method="post" action="index.php?page=accounts&action=login">
+        <p><input type="text" name="login" value="" placeholder="Username or Email"></p>
+        <p><input type="password" name="password" value="" placeholder="Password"></p>
+        <p class="remember_me">
+          <label>
+            <input type="checkbox" name="remember_me" id="remember_me">
+            Remember me on this computer
+          </label>
+        </p>
+        <p class="submit"><input type="submit" name="commit" value="Login"></p>
+      </form>
     </div>
-
-
-</form>
-<form action="index.php?page=accounts&action=signup" method="POST">
-
-    <div class="container">
-
-        <label><b>Not a member yet?</b></label><button type="submit">Signup</button>
+<!-- <form action="index.php?page=accounts&action=signup" method="POST"> -->
+    <div class="login-help">
+      <p>Forgot your password? <a href="index.html">Click here to reset it</a>.</p>
     </div>
+  </section>
 
-
-</form>
-
-
-<script src="js/scripts.js"></script>
+  <section class="about">
+    <p class="about-links">
+      <a href="http://www.cssflow.com/snippets/login-form" target="_parent">View Article</a>
+      <a href="http://www.cssflow.com/snippets/login-form.zip" target="_parent">Download</a>
+    </p>
+    <p class="about-author">
+      &copy;2017 <a href="https://remote.com/himanshuhunge" target="_blank">Himanshu Hunge</a> -
+      <a href="http://www.cssflow.com/mit-license" target="_blank">NJIT License</a><br>
+      Instructed by <a href="http://informatics.njit.edu/people/kwilliam/" target="_blank">Keith Williams</a>
+  </section>
 </body>
 </html>
