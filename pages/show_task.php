336e00775e39a6627a87295bccb9b73ab27007a7
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-05 10:27:11 -0500

    task added

--- pages/show_task.php
+++ pages/show_task.php
@@ -19,10 +19,38 @@
 
 <?php
 //this is how you print something  $data contains the record that was selected on the table.
-print_r($data);
+//print_r($data);
 ?>
-<form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
-    <button type="submit" form="form1" value="delete">Delete</button>
+
+<form action="index.php?page=tasks&action=update&id=<?php echo $data->id; ?> " method="post" id="form1">
+
+<label><b>Owner Email</b></label>
+<input type="text" name="owneremail" value="<?php echo $data->owneremail; ?>" required></br></br>
+
+<label><b>Owner ID</b></label>
+<input type="text" name="ownerid" value="<?php echo $data->ownerid; ?>"></br></br>
+
+<label><b>Created Date</b></label>
+<input type="text" name="createddate" value="<?php echo $data->createddate; ?>"></br></br>
+
+<label><b>Due Date</b></label>
+<input type="text" name="duedate" value="<?php echo $data->duedate; ?>"></br></br>
+
+<label><b>Message</b></label>
+<input type="text" name="message" value="<?php echo $data->message; ?>"></br></br>
+
+<label><b>isDone</b></label>
+<input type="text" name="isdone" value="<?php echo $data->isdone; ?>" ></br></br>
+
+
+    <button type="submit" form="form1" value="delete">Update</button>
+</form>
+
+
+<form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form2">
+
+
+    <button type="submit" form="form2" value="delete">Delete</button>
 </form>
 
 
