e07ac03acbd4abdbdec6e9fa85a96738be80ddf6
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-05 11:23:33 -0500

    checking without bcrypt credentials

--- index.php
+++ index.php
@@ -1,239 +1,52 @@
 <?php
-
-
-define('DATABASE', 'hh292');
-define('USERNAME', 'hh292');
-define('PASSWORD', 'ic2BQ414k');
-define('CONNECTION', 'sql1.njit.edu');
-class Manage {
-    public static function autoload($class) {
-        //you can put any file name or directory here
-        include $class . '.php';
-    }
-}
-spl_autoload_register(array('Manage', 'autoload'));
-$obj=new displayHtml;
-$obj=new main();
-class dbConn{
-    //variable to hold connection object.
-    protected static $db;
-    //private construct - class cannot be instatiated externally.
-    private function __construct() {
-        try {
-            // assign PDO object to db variable
-            self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
-            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-        }
-        catch (PDOException $e) {
-            //Output error - would normally log this to error file rather than output to user.
-            echo "Connection Error: " . $e->getMessage();
-        }
-    }
-    // get connection function. Static method - accessible without instantiation
-    public static function getConnection() {
-        //Guarantees single instance, if no connection object exists then create one.
-        if (!self::$db) {
-            //new connection object.
-            new dbConn();
-        }
-        //
-        //return connection.
-        return self::$db;
-    }
-}
-// ---------------------abstract class for collection ------------------hh292
-abstract class collection {
-protected $html;
-    static public function create() {
-      $model = new static::$modelName;
-      return $model;
-    }
-    static public function findAll() {
-        $db = dbConn::getConnection();
-        $tableName = get_called_class();
-        $sql = 'SELECT * FROM ' . $tableName;
-        $statement = $db->prepare($sql);
-        $statement->execute();
-        $class = static::$modelName;
-        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
-        $recordsSet =  $statement->fetchAll();
-        return $recordsSet;
-    }
-    static public function findOne($id) {
-        $db = dbConn::getConnection();
-        $tableName = get_called_class();
-        $sql = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
-        $statement = $db->prepare($sql);
-        $statement->execute();
-        $class = static::$modelName;
-        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
-        $recordsSet =  $statement->fetchAll();
-        //print_r($recordsSet);
-        return $recordsSet[0];
-    }
-}
-class accounts extends collection {
-    protected static $modelName = 'account';
-}
-class todos extends collection {
-    protected static $modelName = 'todo';
-}
-//-------------------abstract class for model--------------hh292 
-abstract class model {
-protected $tableName;
-public function save()
-    {
-        if ($this->id != '') {
-            $sql = $this->update();
-        } else {
-           $sql = $this->insert();
-        }
-        $db = dbConn::getConnection();
-        $statement = $db->prepare($sql);
-        $array = get_object_vars($this);
-        foreach (array_flip($array) as $key=>$value){
-            $statement->bindParam(":$value", $this->$value);
-        }
-        $statement->execute();
-        $id = $db->lastInsertId();
-        return $id;
-    }
-private function insert() 
-    {      
-        $modelName=get_called_class();
-        $tableName = $modelName::getTablename();
-        $array = get_object_vars($this);
-        $columnString = implode(',', array_flip($array));
-        $valueString = ':'.implode(',:', array_flip($array));
-        //print_r($columnString);
-        $sql =  'INSERT INTO '.$tableName.' ('.$columnString.') VALUES ('.$valueString.')';
-        return $sql;
-    }
-private function update() 
-    {  
-    $modelName=get_called_class();
-    $tableName = $modelName::getTablename();
-    $array = get_object_vars($this);
-    $comma = " ";
-    $sql = 'UPDATE '.$tableName.' SET ';
-    foreach ($array as $key=>$value)
-    {
-        if( ! empty($value)) {
-            $sql .= $comma . $key . ' = "'. $value .'"';
-            $comma = ", ";
-            }
-        }
-        $sql .= ' WHERE id='.$this->id;
-    return $sql;
-    }
-    
-public function delete() 
-    {
-    //echo"In delete";
-    $db = dbConn::getConnection();
-    $modelName=get_called_class();
-    $tableName = $modelName::getTablename();
-    $sql = 'DELETE FROM '.$tableName.' WHERE id ='.$this->id;
-    $statement = $db->prepare($sql);
-    //print_r($sql);
-    $statement->execute();
-    }
-}
-    
-//--------------------------- Account extends Model class -------hh292
-class account extends model {
-    public $id;
-    public $email;
-    public $fname;
-    public $lname;
-    public $phone;
-    public $birthday;
-    public $gender;
-    public $password;
-    public static function getTablename(){
-        $tableName='accounts';
-        return $tableName;
-    }
-}
-//------------------------------todo extends Model class --------hh292
-class todo extends model {
-    public $id;
-    public $owneremail;
-    public $ownerid;
-    public $createddate;
-    public $duedate;
-    public $message;
-    public $isdone;
-    public static function getTablename(){
-        $tableName='todos';
-        return $tableName;
-    }
-} 
-//----------------------------- Main Class ---------------------hh292
-class main
-{
-   public function __construct()
-   {
-
-//---------------------**** Todo Table ****-----------------------------------------hh292
-//---------------------1) Find All Records from todo Table-------------------hh292
-    //$form .= '<br><hr></br>';
-    $form .= '<h1> 2) Todos Table</h1>';
-    $form .= '<h2> A) All records in Todos Table</h2>';
-    $records = todos::findAll();
-    $html = displayHtml::tableDisplayFunction($records); 
-    $form .='<center>'.$html.'</center><hr>';
-//----------------------2) Find Unique Record from todo Table---------------hh292
-    $id = 7;
-    $records = todos::findOne($id);
-    $html = displayHtml::tableDisplayFunction_1($records);
-    $form .='<h2>B) Select One Record</h2>';
-    $form .='<h3> Record fetched with id: <i>'.$id.'</i></h3>';
-    $form .='<center>'.$html.'</center><hr>';
-//--------------- --------3) Insert Record in todo Table-----------------hh292
-    $form .="<h2> C) Insert One Record</h2>";
-    $record = new todo();
-    $record->owneremail="hh292@njit.edu";
-    $record->ownerid=06;
-    $record->createddate="11-05-2017";
-    $record->duedate="11-13-2017";
-    $record->message="Active record Assignment";
-    $record->isdone=1;
-    $lstId=$record->save();
-    $records = todos::findAll();
-    //echo"<h3>After Inserting</h3>";
-    $form .="<h3>Record inserted with id - <i>".$lstId."</i></h3>";
-    $html = displayHtml::tableDisplayFunction($records);
-    $form .='<h3>After inserting the record - </h3>';
-    $form .='<center>'.$html.'</center><hr>';
-//------------------------4) Update Record in todo Table--------------------hh292 
-    $form .="<h2> D) Update Record</h2>";
-    //$id=41;
-    $records = todos::findOne($lstId);
-    $record = new todo();
-    $record->id=$records->id;
-    $record->owneremail="updatetest@njit.edu";
-    $record->message="Update Active record ";
-    $record->save();
-    $records = todos::findAll();
-    $form .="<h3>Updateing record having id: <i>".$lstId."</i></h3>";
-    $html = displayHtml::tableDisplayFunction($records);
-    $form .='<center>'.$html.'</center><hr>';
-// -----------------------5) Delete Record in todo Table------------------hh292
-    $form .= "<h2> E) Delete One Record</h2>";
-    //print_r($lstId);
-    $records = todos::findOne($lstId);
-    $record= new todo();
-    $record->id=$records->id;
-    //print_r($records);
-    $records->delete();
-    $form .='<h3>Record with id: <i>'.$records->id.'</i> is deleted</h3>';
-    //echo "After Delete";
-    $records = todos::findAll();
-    $html = displayHtml::tableDisplayFunction($records);
-    $form .="<h3>After Deleteing</h3>";
-    $form .='<center>'.$html.'</center><hr>';
-    $form .='</form>';
-    print_r($form);
-    }
-}
+//turn on debugging messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+//Autoloader class to load all the different directories
+include_once "autoload.php";
+//put your database credentials here
+include_once "database.php";
+//this starts the program as a static.  Start tracing the program from here following the classes and methods being called
+$response = http\processRequest::createResponse();
+//read the notes below
+//To make the final project, you need to add routes, controllers, and the html templates in the "pages" folder.
+//One of the big challenges will be to get the tasks to show up for the user that created them.
+//Basic Steps:
+//1. Add a route for each request you need to process (GET / POST).  See the template provided in routes.php.
+//2. Add a controller and/or add methods to the controller, which match the actions that you want the program to perform.
+//3. Add HTML view templates to the page folder.  Look at how the template is called and you can pass data to the template
+//Suggested order of work:
+//1.  Get findall working and displaying a table for the todos class's todos_list method;
+//2.  Get findOne working to find one to-do and make that work for the todos controller show method.  Remember you have to pass the ID.
+//3.  Get the Insert working
+//4.  get the delete working
+//5.  update working
+//6.  once you have this all working for todos start working on accounts
+//7.  once accounts works for login / logout / show user profile / edit profile
+//8.  go back and add a useriD field to your todos table and update program accordingly i.e. the model
+//9.  add a method to your to-do model that retrieves by userID instead of ID.  USe the findONe as an example
+//10.  on your To-do-List method of the todos controller update it so that it takes the USER id out of the session and uses that to retrieve the todos
+//to get credit for using this as MVC you must rewrite what I give and improve it.
+//  A good way to improve it is namespaces and making the scope of properties and functions to be correctly private, public, or protected
+//there are notes throughout the code on improvements.  YOu can also correctly apply abstract and final
+//you can also look for lines that can be removed by just doing it in the return
+//it shouldn't be too hard to namespace and autoload
+//namespaces are really needed because your collection and controller classes for todos and accounts are called the same thing.
+//IMPORTANT:  YOUR ACTIVE RECORD collection CLASSES  (task/account) WILL CONFLICT WITH THE CONTROLER CLASS HERE.
+//You can use namespaces or rename the controller classes, which will change your url parameter for page
+//routes are used to match the http request with the controller and method name that are called for that request / route.
+//In this program the page parameter matches the controller and the action parameter matches the method on the conroller.
+//Examples:
+//  index.php?page=todos&action=show specifies the todos collection class and the show method/function
+//  GET requests to show the  form for new todos should go to index.php?page=todos&action=create and show a form for a new todoItem
+//  POST requests to create todos should go to index.php?page=todos&action=store and would be inserted into the database
+//  GET requests to show the update form todos should go to index.php?page=todos&action=edit&id=(todoItem ID) and would show an update form
+//  POST requests to update todos should go to index.php?page=todos&action=store&id=(todoItem ID) and would be update in the database
+//  For delete you should put a delete button within a new form below the edit form that has a method post and action of index.php?page=todos&action=delete&id=(todoItem ID)
+//You can use the accounts controller for handling user login, user registration, showing the profile, editing the profile, and logout
+//Login should hash the password and compare it to the saved password hash
+//registration should hash the password and insert the hashed password with the user record.  You should not store clear passwords in a database
+//for the todos list page you need to make a table that has links to each item.
+//  Like:  index.php?page=todos&action=show&id=1  this would show the to-do item with a link to the edit form.
+//  you could put your delete on the to_do item view or the edit form, the above still applies.
+?>
