e8a67160f3ee544de9ed8e9baa9bf5d459adb2b7
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-04 19:02:24 -0500

    show_task

---
+++ pages/show_task.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<?php
+//this is how you print something  $data contains the record that was selected on the table.
+print_r($data);
+?>
+<form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
+    <button type="submit" form="form1" value="delete">Delete</button>
+</form>
+
+
+
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
