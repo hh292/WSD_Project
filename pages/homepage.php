3e12963b3c41e77e56d03cdecc0a182438827d9c
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-05 10:24:12 -0500

    updated file

--- pages/homepage.php
+++ pages/homepage.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>WSD Final Project</title>
+    <meta name="description" content="WSD Final Project">
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
@@ -21,22 +21,32 @@
 <h1>
     <?php
     //this how to print some data;
-    echo $data['site_name'];
+    //echo $data['site_name'];
     ?> </h1>
 
-<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
-<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
+<!--<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
+<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>-->
 
 <form action="index.php?page=accounts&action=login" method="POST">
 
     <div class="container">
         <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required>
+        <input type="text" placeholder="Enter Username" name="uname" required></br></br>
 
         <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+        <input type="password" placeholder="Enter Password" name="psw" required></br></br>
 
-        <button type="submit">Login</button>
+        <button type="submit">Login</button></br></br>
+
+    </div>
+
+
+</form>
+<form action="index.php?page=accounts&action=signup" method="POST">
+
+    <div class="container">
+
+        <label><b>Not a member yet?</b></label><button type="submit">Signup</button>
     </div>
 
 
