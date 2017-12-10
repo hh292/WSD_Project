4ddb670b2727bae450e43a9547f1247c3d9a7c93
Himanshu Hunge <hh292@afsconnect1.njit.edu>
2017-12-09 22:10:40 -0500

    Update

--- pages/edit_task.php
+++ pages/edit_task.php
@@ -1,5 +1,4 @@
-put a page with in a form to edit a task and have a delete button in another form to delete
-
+//put a page with in a form to edit a task and have a delete button in another form to delete
 <form action="index.php?page=tasks&action=delete&id=<?php $data['id'] ?> " method="post" id="form1">
     <button type="submit" form="form1" value="delete">Delete</button>
 </form>
