9aad743a5d8fe62c3cbbe7bdc851022f67b48c21
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-13 20:27:23 -0500

    final changes

--- pages/notfound.php
+++ pages/notfound.php
@@ -4,19 +4,15 @@
 <head>
     <meta charset="utf-8">
 
-    <title>WSD Project 2</title>
+    <title>WSD Project - FALL2017</title>
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
 
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
 </head>
 
 <body>
 
 <h1>Page Not Found 404</h1>
-
 
 <script src="js/scripts.js"></script>
 </body>
