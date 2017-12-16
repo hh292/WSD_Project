f83f91b8c0b7d5e4d87c60693f0da543fc599be5
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-16 14:19:41 -0500

    changes in front UI

--- pages/password_change.php
+++ pages/password_change.php
@@ -1,42 +1,51 @@
 <?php include 'headerPage.php' ?>
 
-<?php
-//this is how you print something  $data contains the record that was selected on the table.
-//print_r($data);
-?>
 
-<form action="index.php?page=accounts&action=updatePass&id=<?php echo $data->id; ?> " method="post" id="form1">
-
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-2">
-                <label><b>Enter your current password:</b></label>
-            </div>
-            <div class="col-lg-1">
-                <input type="text" name="currentPass" required>
-            </div>
+<div class="content-inner">
+    <!-- Page Header-->
+    <header class="page-header">
+        <div class="container-fluid">
+            <h2 class="no-margin-bottom">Password Update</h2>
         </div>
-        <div class="row">
-            <div class="col-lg-2">
-                <label><b>Enter New Password</b></label>
-            </div>
-            <div class="col-lg-1">
-                <input type="text" name="newPass1">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-2">
-                <label><b>Re-enter Password</b></label>
-            </div>
-            <div class="col-lg-1">
-                <input type="text" name="newPass2">
-            </div>
-        </div>
-
+    </header>
+    <!-- Breadcrumb-->
+    <div class="breadcrumb-holder container-fluid">
+        <ul class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.php?page=tasks&action=allOneUser&id=<?php echo $_SESSION["userID"] ?>"">Home</a></li>
+            <li class="breadcrumb-item active">Password Update</li>
+        </ul>
     </div>
-    <div class="col-lg-2"><button class="btn btn-primary" type="submit" form="form1" value="save">Update Password</button><a href="index.php?page=accounts&action=showProf">Cancel</a></div>
-</form>
-
-<script src="js/scripts.js"></script>
-</body>
-</html>
+    <!-- Dashboard Counts Section-->
+    <section class="forms">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="line"></div>
+                            <form class="form-horizontal" action="index.php?page=accounts&action=updatePass&id=<?php echo $data->id; ?> " method="post" id="form1">
+                                <div class="form-group row">
+                                    <label class="col-sm-3 form-control-label">Enter Your Current Password</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" name="currentPass" required>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-3 form-control-label">Enter New Password</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" name="newPass1">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-3 form-control-label">Re-enter Password</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" name="newPass2">
+                                    </div>
+                                </div>
+                                <div class="line"></div>
+                                <div class="col-lg-4">
+                                    <div class="col-lg-2"><button class="btn btn-primary" type="submit" form="form1" value="save">Update Password</button><a href="index.php?page=accounts&action=showProf">Cancel</a></div>
+                            </form>
+                            <script src="js/scripts.js"></script>
+                            </body>
+                 </html>
