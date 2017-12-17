6985bd1bae15261433810ca17fab80765f4c7928
Himanshu Hunge <hh292@afsconnect2.njit.edu>
2017-12-16 22:05:58 -0500

    Final code formatting

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -1,11 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kwilliams
- * Date: 11/27/17
- * Time: 5:32 PM
- */
-//each page extends controller and the index.php?page=tasks causes the controller to be called
+
+// .........................Himanshu Hunge hh292
+
 class accountsController extends http\controller
 {
     //each method in the controller is named an action.
@@ -25,10 +21,7 @@ class accountsController extends http\controller
     {
         self::getTemplate('new_user');
     }
-    //to call the show function the url is called with a post to: index.php?page=task&action=create
-    //this is a function to create new tasks
-    //you should check the notes on the project posted in moodle for how to use active record here
-    //this is to register an account i.e. insert a new account
+    
     public static function register()
 {
     //https://www.sitepoint.com/why-you-should-use-bcrypt-to-hash-stored-passwords/
@@ -42,9 +35,7 @@ class accountsController extends http\controller
         $record->phone = $_POST['phone'];
         $record->birthday = $_POST['bday'];
         $record->gender = $_POST['gender'];
-        //$record->password = $_POST['password'];
         $record->password = $record->setPassword($_POST['password']);
-        //print_r($record);
         $record->save();
         header('Location: index.php');
     }
@@ -107,26 +98,17 @@ class accountsController extends http\controller
             echo 'Wrong password entered.';
         }
     }
+    //.......................Himanshu Hunge hh292
     //this is to login, here is where you find the account and allow login or deny.
     public static function login()
     {
-        //you will need to fix this so we can find users username.  YOu should add this method findUser to the accounts collection
-        //when you add the method you need to look at my find one, you need to return the user object.
-        //then you need to check the password and create the session if the password matches.
-        //you might want to add something that handles if the password is invalid, you could add a page template and direct to that
-        //after you login you can use the header function to forward the user to a page that displays their tasks.
-        //        $record = accounts::findUser($_POST['uname']);
         $record = new account();
         $record = accounts::findUserbyUsername($_POST['login']);
-        //$checkpsw = accounts::checkPassword($_POST['psw'],$record->password);
-        //print_r($record);
-        //echo '1';
+        
         if ($record == FALSE) {
-            //header('Location: index.php');
             print_r("<h1>'Sorry! User not found,Please enter correct Login credentials....!'</h1>");
         } else {
             if($record->checkPassword($_POST['psw']) == TRUE) {
-                //echo 'login';
                 session_start();
                 $_SESSION["userID"] = $record->id;
                 $_SESSION["userEmail"] = $record->email;
